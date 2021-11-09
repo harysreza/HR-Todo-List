43d802bbf804c706ea9a11d76e1488fce1b50f7b
harysreza <harysreza21@gmail.com>
2021-11-09 14:13:11 +0700

    Fix word break

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -5,7 +5,6 @@ function Icons({ todo, update, remove, onToggle }) {
     <div className="icons">
       <AiFillCheckSquare onClick={() => onToggle(todo.id)} style={{ color: "green", cursor: "pointer" }} size="1.6rem" />
       <AiFillEdit onClick={() => update(todo)} style={{ color: "blue", cursor: "pointer" }} size="1.6rem" />
-      {/* <RiStarLine /> */}
       <AiFillCloseSquare onClick={() => remove(todo.id)} style={{ color: "red", cursor: "pointer" }} size="1.6rem" />
     </div>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,9 +3,8 @@ import Icons from "./Icons";
 function Todo({ todo, update, remove, onToggle, setEditTodo }) {
   return (
     <div className={`todo ${todo.complete ? "complete" : ""}`}>
-      <h3>
-        {todo.text} <Icons update={update} remove={remove} todo={todo} setEditTodo={setEditTodo} onToggle={onToggle} />
-      </h3>
+      <h3>{todo.text}</h3>
+      <Icons update={update} remove={remove} todo={todo} setEditTodo={setEditTodo} onToggle={onToggle} />
     </div>
   );
 }
